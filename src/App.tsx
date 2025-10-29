dbe7c14ea00c51e8b833a9eea8bfc86ad6c859ac
ikhsan Zuhri Al Ghifary <zuhrialghifaryikhsan@gmail.com>
2025-10-30 01:39:18 +0700

    feat: prod monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Layout } from "./components/Layout";
 import Index from "./pages/Index";
 import Post from "./pages/Post";
@@ -23,6 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <SpeedInsights />
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
