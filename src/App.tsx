c996e15dc96754dec9d4dfd2c08066f183b25aa9
ikhsan Zuhri Al Ghifary <zuhrialghifaryikhsan@gmail.com>
2025-10-30 13:10:34 +0700

    feat: update aanlytics vercel

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import { Layout } from "./components/Layout";
 import Index from "./pages/Index";
 import Post from "./pages/Post";
@@ -25,6 +26,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <SpeedInsights />
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
